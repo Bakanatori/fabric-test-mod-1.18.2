d9fcd5cbf30d5861e1bea054d102b430890ff3b4
notbaka <lildeadbaka@yandex.ru>
2023-02-18 02:18:01 +0300

    Added new block and items + textures for them.
    List of items and blocks:
    - mythril ingot
    - mythril nugget
    - mythril ore
    - mythril block
    - raw mythril block
    - deepslate mythril ore
    - netherrack mythril ore
    
    actually testing all of these

--- src/main/java/net/notbaka/tutorialmod/TutorialMod.java
+++ src/main/java/net/notbaka/tutorialmod/TutorialMod.java
@@ -1,14 +1,19 @@
 package net.notbaka.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.notbaka.tutorialmod.block.ModBlocks;
+import net.notbaka.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID= "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
 	@Override
 	public void onInitialize() {
 
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
