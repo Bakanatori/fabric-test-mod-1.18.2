97cefc216a5df93e14c0da059eb00b13e17bd4c9
notbaka <lildeadbaka@yandex.ru>
2023-02-18 01:14:22 +0300

    Initial commit

---
+++ src/main/java/net/notbaka/tutorialmod/TutorialMod.java
@@ -0,0 +1,14 @@
+package net.notbaka.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
